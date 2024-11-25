a835bfec7915614664edeea4b02c2dd594d5a3fe
Winner <158045429+codinglife108@users.noreply.github.com>
2024-11-25 11:54:44 -0400

    feat: update initData

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -10,8 +10,8 @@ const useApi = (route: string, options: any = {}) => {
     useEffect(() => {
         const fetchData = async () => {
             // @ts-ignore
-            const initData = window["Telegram"]['WebApp']['initData'];
-            // const initData = "query_id=AAGVFt08AwAAAJUW3TwV15BE&user=%7B%22id%22%3A7463573141%2C%22first_name%22%3A%22Abel%22%2C%22last_name%22%3A%22Tony%22%2C%22username%22%3A%22ProScriptSlinger4%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727671559&hash=2d32a0b1f98969c72bb9288ca8167f86605d5a2df2d4026af5166a21b8e4d19d";
+            // const initData = window["Telegram"]['WebApp']['initData'];
+            const initData = "query_id=AAGVFt08AwAAAJUW3TwV15BE&user=%7B%22id%22%3A7463573141%2C%22first_name%22%3A%22Abel%22%2C%22last_name%22%3A%22Tony%22%2C%22username%22%3A%22ProScriptSlinger4%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1727671559&hash=2d32a0b1f98969c72bb9288ca8167f86605d5a2df2d4026af5166a21b8e4d19d";
             const url = getUrl() + route;
 
             try {
