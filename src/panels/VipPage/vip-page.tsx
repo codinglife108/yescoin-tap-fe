6748284db9504c1a45b3696e5b0c51c960b71b4b
Winner <158045429+codinglife108@users.noreply.github.com>
2024-12-02 17:42:21 -0400

    chor: vip page

--- src/panels/VipPage/vip-page.tsx
+++ src/panels/VipPage/vip-page.tsx
@@ -49,12 +49,12 @@ const PlayerRow = ({ rank, username, invite_count, gold }: IPlayerProps) => {
                             <div className={
                                 rank === 1
                                     ? 'visible rounded-50px w-[85px] h-[85px] bg-gray-600 rounded-full flex items-center justify-center'
-                                    : 'visible rounded-50px w-[64px] h-[64px] bg-gray-600 rounded-full flex items-center justify-center'
+                                    : 'visible rounded-50px w-[44px] h-[44px] bg-gray-600 rounded-full flex items-center justify-center'
                             }>
                                 <span className={"text-xl"}>{username.charAt(0).toUpperCase()}</span>
                             </div>
                             <div
-                                className={`p-[5px] w-[25px] h-[25px] flex justify-center items-center text-center rounded-full font-semibold -mt-[15px] ${getRankColor(
+                                className={`max-w-[20px] h-[18px] text-[9px] font-semibold leading-[10px] p-[10px] flex justify-center items-center text-center rounded-full -mt-[15px] ${getRankColor(
                                     rank
                                 )}`}
                             >
@@ -62,27 +62,27 @@ const PlayerRow = ({ rank, username, invite_count, gold }: IPlayerProps) => {
                             </div>
                         </div>
                         <div className='flex flex-col justify-center items-start gap-[6px]'>
-                            <span className='text-white font-bold text-[1rem]'>
+                            <span className='text-white font-medium leading-[15.73px] text-[13px]'>
                                 {username}
                             </span>
                             <div className='flex justify-center items-center gap-[5px] text-[15px] font-normal justify-content-flex-start'>
                                 <Img
                                     src={require('../../assets/images/vip/user.png')}
-                                    className='w-[25px] h-[25px]'
+                                    className='w-[15px] h-[15px]'
                                 />
-                                <div>
+                                <p className='text-white text-[13px] font-normal leading-[15.73px]'>
                                     {truncateNumber(
                                         parseInt(invite_count.toString(), 10)
                                     )} frens
-                                </div>
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div className='pt-[5px] text-[16px]'>
+                    <div className='pt-[5px] text-white text-[13px] leading-[15.73px] font-medium text-nowrap'>
                         {RewardBalances.positions[rank]}
                     </div>
                 </div>
-                <hr style={{ borderColor: '#444447' }}></hr>
+                <hr style={{ borderColor: '#38383B' }}></hr>
             </div>
         </>
     )
@@ -94,30 +94,30 @@ const PlayerItem = ({ rank, username, invite_count, gold }: IPlayerProps) => {
             <div className='grid place-items-center justify-center'>
                 <div className={
                     rank === 1
-                        ? 'visible rounded-50px w-[85px] h-[85px] bg-gray-600 rounded-full flex items-center justify-center'
+                        ? 'visible rounded-50px w-[90px] h-[90px] bg-gray-600 rounded-full flex items-center justify-center'
                         : 'visible rounded-50px w-[64px] h-[64px] bg-gray-600 rounded-full flex items-center justify-center'
                 }>
                     <span className={"text-xl"}>{username.charAt(0).toUpperCase()}</span>
                 </div>
                 <p
-                    className={`p-[5px] w-[25px] h-[25px] flex justify-center items-center text-center rounded-full font-semibold -mt-[15px] ${getRankColor(
+                    className={`w-[20px] h-[20px] text-xs flex justify-center items-center text-center rounded-full font-medium -mt-[15px] ${getRankColor(
                         rank
                     )}`}
                 >
                     {rank}
                 </p>
             </div>
-            <div className='pt-2.5 text-[20px] font-bold'>{username}</div>
-            <div className='flex justify-center items-center gap-[5px] text-[15px] font-normal pt-3.5'>
+            <div className='pt-1 text-[13px] leading-[15.73px] font-bold'>{username}</div>
+            <div className='flex justify-center items-center gap-[5px] text-[10px] font-medium text-xs pt-1 leading-[12.1px]'>
                 <Img
                     src={require('../../assets/images/vip/user.png')}
-                    className='w-[25px] h-[25px]'
+                    className='w-[15px] h-[15px]'
                 />
                 <div>
                     {truncateNumber(parseInt(invite_count.toString(), 10))} frens
                 </div>
             </div>
-            <div>{RewardBalances.positions[rank]}</div>
+            <p className='text-xs text-white font-medium leading-[14.52px]'>{RewardBalances.positions[rank]}</p>
         </div>
     )
 }
@@ -175,14 +175,12 @@ export default function VipPage() {
                     vertical='bottom'
                     fromBottom
                 />
-                <br />
-                <br />
                 <TelegramBackButton />
                 {loading ? (
                     <FriendsListSkeleton />
                 ) : leaderboardData.length > 0 ? (
                     <>
-                        <div className='flex justify-around relative'>
+                        <div className='flex justify-around relative mt-[30px] px-[23px]'>
                             <div className='w-1/3 grid place-items-center text-center pt-6'>
                                 <PlayerItem
                                     key={leaderboardData[1].tgid}
@@ -223,7 +221,7 @@ export default function VipPage() {
                                 )}
                             </div>
                         </div>
-                        <div className='p-[1rem] pt-[3rem] relative'>
+                        <div className='mt-[30px] mx-[20px] relative'>
                             <div
                                 className='bg-cover bg-center grid place-content-center place-items-center rounded-[10px] gap-[8px] p-[20px]'
                                 style={{
@@ -235,14 +233,14 @@ export default function VipPage() {
                                         src={require('../../assets/images/vip/star5.png')}
                                         className='absolute w-[30px] left-[-45px] rounded-none'
                                     />
-                                    <div className='text-[21px] font-bold grid content-center items-center justify-center'>
+                                    <p className='text-lg font-medium text-white leading-[21.78px] text-center grid content-center items-center justify-center'>
                                         The Top 300 Leaders
-                                    </div>
+                                    </p>
                                 </div>
-                                <div className='text-[19px] font-normal grid content-center items-center justify-center'>
+                                <div className='text-[13px] leading-[15.73px] font-normal text-white grid content-center items-center justify-center'>
                                     will get a share of
                                 </div>
-                                <div className='text-[19px] font-normal flex relative'>
+                                <div className='mt-[10px] font-normal flex relative'>
                                     <Img
                                         src={require('../../assets/images/vip/amount.png')}
                                     />
@@ -253,10 +251,10 @@ export default function VipPage() {
                                 </div>
                             </div>
                         </div>
-                        <div className='bg-[#2c2c2e] rounded-tl-[16px] rounded-tr-[16px] mt-4 relative'>
-                            <div className='text-center py-[1.5rem] border-b-[1px] border-[#444447] grid text-[25px] font-bold'>
+                        <div className='bg-[#2c2c2e] rounded-tl-[16px] rounded-tr-[16px] mt-[30px] relative'>
+                            <p className='text-center py-[20px] border-b-[1px] border-[#444447] grid text-lg font-medium leading-[21.78px]'>
                                 Top Leaders
-                            </div>
+                            </p>
                             {leaderboardData
                                 .slice(3, visiblePlayers)
                                 .map((player, i) => (
